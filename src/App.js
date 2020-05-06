4e497beb11815f7bbf5822a812a4f87a3e8baf5b
Birte Spreuer <mail@birtespreuer.com>
2020-05-06 19:42:20 +0200

    Footer

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Router, Route, Switch } from "react-router";
 import Header from "./component/header/Header";
 import "./stylesheets/global.scss";
 import Highlight from "./component/highlights/Highlights";
+import Footer from "./component/footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Highlight />
+      <Footer />
     </div>
   );
 }
