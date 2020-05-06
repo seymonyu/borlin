f0f84915e4d65ad2a0e9e54fc680c4802218f5cd
Birte Spreuer <mail@birtespreuer.com>
2020-05-06 16:42:59 +0200

    global styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
 import { Router, Route, Switch } from "react-router";
+import Header from "./component/header/Header";
+import "./stylesheets/global.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
