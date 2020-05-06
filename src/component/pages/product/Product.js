8afa4b907e20eb8255f2736cad4144c02c6a9d2b
Lígia Souza <ligia.o.souza@gmail.com>
2020-05-06 20:31:04 +0200

    change api and create product functionality"

--- src/component/pages/product/Product.js
+++ src/component/pages/product/Product.js
@@ -1,49 +1,70 @@
-import React, {Component} from 'react'
-import {data} from '../../../API/data'
-export default class Product extends Component{
-
-state = {
-    data:data,
+import React, { Component } from "react";
+import { data } from "../../../API/data";
+export default class Product extends Component {
+  state = {
+    data: data,
     selectedData: "",
-    productList:[]
-}
+    productList: [],
+  };
 
-handlerSelectedData = (e)=>{
-    const category = e.target.id
- let newData =[] 
- this.state.data.products.filter(item=> {if(item.category === category)newData.push(item) })
- this.setState({productList:newData})
-}
-handlePriceHighLow =()=>{
-   const sortHighLow = this.state.productList.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
-console.log(sortHighLow)
-}
-handlePriceLowHigh =()=>{
-    const sortLowHigh = this.state.productList.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
- console.log(sortLowHigh)
- }
-    render(){
-        return(
+  handlerSelectedData = (e) => {
+    const category = e.target.id;
+    let newData = [];
+    this.state.data.products.filter((item) => {
+      if (item.category === category) newData.push(item);
+    });
+    this.setState({ productList: newData });
+  };
 
-<div>
-    <ul>
-        <li> <button id="party" onClick={this.handlerSelectedData}> Party</button></li>
-        <li>lounge</li>
-        <li>casual</li>
-        <li>evening</li>
-        <li>active</li>
-    </ul>
+  handleprices = (e) => {
+    const action = e.target.value;
+    const sortHighLow = this.state.productList.sort(
+      (a, b) => parseFloat(b.price) - parseFloat(a.price)
+    );
+    const sortLowHigh = this.state.productList.sort(
+      (a, b) => parseFloat(a.price) - parseFloat(b.price)
+    );
+    this.setState({
+      sortValue: action === "ASC" ? sortLowHigh : sortHighLow,
+    });
 
-    <button onClick={this.handlePriceHighLow}>High to Low</button>
-    <button onClick={this.handlePriceLowHigh}> Low High</button>
+    console.log(sortHighLow);
+  };
 
-{this.state.productList.map((product,i) =>
-    <div key={i}>
-    <p>{product.price}</p>
-<img src={product.image} alt={product.id}/>
-</div>
-)}
-</div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>
+            {" "}
+            <button id="party" onClick={this.handlerSelectedData}>
+              {" "}
+              Party
+            </button>
+          </li>
+          <li>lounge</li>
+          <li>casual</li>
+          <li>evening</li>
+          <li>active</li>
+        </ul>
+
+        <select
+          onChange={this.handleprices}
+          name="selectPrice"
+          id="selectPrice"
+        >
+          <option> Select your option</option>
+          <option value="DES">High to Low</option>
+          <option value="ASC">Low High</option>
+        </select>
+
+        {this.state.productList.map((product, i) => (
+          <div key={i}>
+            <p>{product.price}</p>
+            <img src={product.image} alt={product.id} />
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
