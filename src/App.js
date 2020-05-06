8b8a500fdd8aef8794847d1d95f8f1f82c484ccb
Birte Spreuer <mail@birtespreuer.com>
2020-05-06 21:07:34 +0200

    branch card-birte

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./App.css";
-import { Router, Route, Switch } from "react-router";
 import Header from "./component/header/Header";
-import "./stylesheets/global.scss";
 import Highlight from "./component/highlights/Highlights";
+import "./stylesheets/global.scss";
+import Navbar from "./component/navbar/Navbar";
+import NewArrivals from "./component/newarrivals/NewArrivals";
 import Footer from "./component/footer/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Header />
       <Highlight />
+      <NewArrivals />
       <Footer />
     </div>
   );
