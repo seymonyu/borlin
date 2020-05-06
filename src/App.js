bce557d7364b919e5bb646da88d3db416c976f5f
seymonyu <59199817+seymonyu@users.noreply.github.com>
2020-05-06 18:35:00 +0200

    highlights from birt

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { Router, Route, Switch } from "react-router";
 import Header from "./component/header/Header";
+import Highlight from "./component/highlights/Highlights";
 import "./stylesheets/global.scss";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Highlight />
     </div>
   );
 }
