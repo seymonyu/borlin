5775f7fa19f87ff11b4691f781c08e251f09fa74
Birte Spreuer <mail@birtespreuer.com>
2020-05-06 18:17:27 +0200

    Highlights create and style

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import { Router, Route, Switch } from "react-router";
 import Header from "./component/header/Header";
 import "./stylesheets/global.scss";
+import Highlight from "./component/highlights/Highlights";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Highlight />
     </div>
   );
 }
