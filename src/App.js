f41d3cb3398a0bf5eb46dc4ff882097935cbedcc
seymonyu <59199817+seymonyu@users.noreply.github.com>
2020-05-06 21:55:38 +0200

    App changed

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Header from "./component/header/Header";
 import "./stylesheets/global.scss";
 import Highlight from "./component/highlights/Highlights";
 import Footer from "./component/footer/Footer";
+import NewArrivals from "./component/newarrivals/NewArrivals";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Highlight />
+      <NewArrivals />
       <Footer />
     </div>
   );
