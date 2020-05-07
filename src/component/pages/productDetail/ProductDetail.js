b2f42caff7092f018d20132a2ff48955670514e2
Elsa Desta <elsamenelik@gmail.com>
2020-05-07 22:22:59 +0200

    adjusting functionality

--- src/component/pages/productDetail/ProductDetail.js
+++ src/component/pages/productDetail/ProductDetail.js
@@ -1,21 +1,18 @@
 import { connect } from "react-redux";
 import { data } from "../../../API/data";
-import {addToCart} from "../../../reducers/action/actions.product";
+import { addToCart } from "../../../reducers/action/actions.product";
 import React, { Component } from "react";
 import "../../../stylesheets/ProductDetail.scss";
 
 class ProductDetail extends Component {
   state = {
-    products: this.props.products,
+    products: this.props.product,
     selectedData: "",
     cartList: [],
     name: this.props.name,
-    total: 0
+    total: 0,
   };
 
-  
-
-  
   /*handleclick to add to cart when clicked*/
 
   handleClick = () => {
@@ -24,51 +21,38 @@ class ProductDetail extends Component {
 
   render() {
     console.log(this.state.products);
-    return ( <div>{this.state.products?   <div className="product_detail--wrapper">
-    <div className="product_detail--header">
-      <h1 className="product_detail--h1">Product Detail</h1>
-    </div>
-    <img
-      src={this.state.products.image} 
-      alt={this.state.products.id}
-    />
-   
-    <p>{this.state.products.category}</p>
-    <p>{this.state.products.size}</p>
-    <p> {this.state.products.price}</p>
-    {/* <button 
-      onClick={() => {
-        this.handleClick(this.state.products.id);
-      }}
-    ></button> */}
-  </div> : <h1>Helloooo</h1>  }</div> 
-      
+    return (
+      <div className="product_detail--wrapper">
+        <div className="product_left--wrapper">
+          <h1 className="product_detail--h1">Product Detail</h1>
+          <div className="product_detail-img_container">
+            <img src={this.state.products.image} alt={this.state.products.id} />{" "}
+            
+          </div>
+        </div>
+
+        <div className="product_detail--right_wrapper">
+          <p>{this.state.products.category}</p>
+          <p>{this.state.products.size}</p>
+          <p> {this.state.products.price}</p>
+        </div>
+
+        <button className="product_detail--button" value={this.state.products.id}
+                   
+        >
+          ADD TO CART
+        </button>
+      </div>
     );
   }
 }
 
 /*connecto to our reducer to change the state of our store  */
 
-
-
 const mapStateToProps = (state) => {
   return {
-    product: state.addShipping
-  }
+    product: state.addShipping,
+  };
+};
 
-}
-
-
-
-
-//   return {
-//     addToCart: () => {
-//       dispatch({type: "ADD_TO_CART"});
-//     },
-//   };
-// };
-
-
-
-
-export default connect(null,mapStateToProps)(ProductDetail);
+export default connect(mapStateToProps)(ProductDetail);
