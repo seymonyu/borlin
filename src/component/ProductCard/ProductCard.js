6d45961b8d8aa155fc7722afbfc6f04dd572b9a8
ChandniKJ <54037953+ChandniKJ@users.noreply.github.com>
2020-05-07 15:35:46 +0100

    Updated API and Card

--- src/component/ProductCard/ProductCard.js
+++ src/component/ProductCard/ProductCard.js
@@ -3,9 +3,8 @@ import { getProduct } from "../../reducers/action/index";
 import { connect } from "react-redux";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Row";
+import "./ProductCard.css";
 
 import { Link } from "react-router-dom";
 
@@ -29,26 +28,26 @@ class ProductCard extends Component {
   render() {
     return (
       <Container className="AllProduct">
-        <Row>
-          <Col>
-            <Card>
-              <Card.Img
-                variant="top"
-                src={this.props.product.image}
-                id={this.props.product.id}
-                style={{ height: "13rem" }}
-                onClick={this.handleDispatch}
-              />
-              <Card.Body>
-                <Card.Title>{this.props.product.name}</Card.Title>
-                <Card.Title>{this.props.product.price}</Card.Title>
-                <Link to={`/productdetails`}>
-                  <Button variant="danger">More Details</Button>
-                </Link>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
+        <Card>
+          <Card.Img
+            variant="top"
+            src={this.props.product.image}
+            id={this.props.product.id}
+            style={{ height: "13rem" }}
+            onClick={this.handleDispatch}
+          />
+
+          <Card.Body>
+            <Card.Title>{this.props.product.name}</Card.Title>
+            <Card.Title>{this.props.product.price}</Card.Title>
+            <Link to={`/productdetails`}>
+              <Button variant="danger">More Details</Button>
+              <figure className="Favorite">
+                <span onClick>&#9733;</span>
+              </figure>
+            </Link>
+          </Card.Body>
+        </Card>
       </Container>
     );
   }
