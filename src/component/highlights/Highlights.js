db62b85a74ce0dcd31ed7d9ae9b0fa995547bde8
seymonyu <59199817+seymonyu@users.noreply.github.com>
2020-05-08 10:31:40 +0200

    seyma cart

--- src/component/highlights/Highlights.js
+++ src/component/highlights/Highlights.js
@@ -1,41 +1,59 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import "../../stylesheets/global.scss";
 import "../../stylesheets/Highlights.scss";
+import cartReducer from "../../reducers/cartReducer";
 
 class Highlight extends Component {
+  state = {
+    data: this.props.data,
+    name: this.props.name,
+  };
   render() {
     return (
       <div className="highlights--wrapper">
         <h2 className="highlight--headline headline-2">Highlights</h2>
         <div className="highlight--product_wrap">
           <div className="highlight--product_cols">
-            <div className="highlight--img"></div>
+            <div className="highlight--img"> </div>
             <div className="highlight--text">
-              <p className="highlight--product_p">Productname</p>
-              <span className="highlight--product_span">125€</span>
+              <p className="highlight--product_p">{this.state.data[0].name}</p>
+              <span className="highlight--product_span">
+                {this.state.data[0].price}€
+              </span>
             </div>
           </div>
           <div className="highlight--product_cols">
             <div className="highlight--img"></div>
             <div className="highlight--text">
-              <p className="highlight--product_p">Productname</p>
-              <span className="highlight--product_span">125€</span>
+              <p className="highlight--product_p">{this.state.data[2].name}</p>
+              <span className="highlight--product_span">
+                {this.state.data[2].price}€
+              </span>
             </div>
           </div>
           <div className="highlight--product_cols">
             <div className="highlight--img"></div>
             <div className="highlight--text">
-              <p className="highlight--product_p">Productname</p>
-              <span className="highlight--product_span">125€</span>
+              <p className="highlight--product_p">{this.state.data[6].name}</p>
+              <span className="highlight--product_span">
+                {this.state.data[6].price}€
+              </span>
             </div>
           </div>
         </div>
         <div className="highlight--button_wrap">
-          <div className="highlight--product_button">show all</div>
+          <button className="highlight--product_button">show all</button>
         </div>
       </div>
     );
   }
 }
 
-export default Highlight;
+const mapStateToProps = (state) => {
+  return {
+    data: state.cartReducer.data,
+  };
+};
+
+export default connect(mapStateToProps)(Highlight);
