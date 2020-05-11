929afc2cf288a9f9f170e6db282beb1f2fb62192
Lígia Souza <ligia.o.souza@gmail.com>
2020-05-11 12:30:15 +0200

    bugs fixed

--- src/component/pages/product/services/services.js
+++ src/component/pages/product/services/services.js
@@ -1,4 +1,4 @@
 export  const asc = (state)=>  state.sort(
-    (a, b) => parseFloat(a.price) - parseFloat(b.price))
+    (a, b) => parseFloat(b.price) - parseFloat(a.price))
  export  const des = (state)=>  state.sort(
         (a, b) => parseFloat(a.price) - parseFloat(b.price))
