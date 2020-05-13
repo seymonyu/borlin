6192a8a143690694c5b2a1a35cf0760a8afd75d3
Lígia Souza <ligia.o.souza@gmail.com>
2020-05-13 17:10:21 +0200

    changing tittle

--- src/component/pages/product/services/services.js
+++ src/component/pages/product/services/services.js
@@ -1,4 +1,4 @@
 export  const asc = (state)=>  state.sort(
-    (a, b) => parseFloat(b.price) - parseFloat(a.price))
+    (a, b) => parseFloat(a.price) - parseFloat(b.price))
  export  const des = (state)=>  state.sort(
-        (a, b) => parseFloat(a.price) - parseFloat(b.price))
+        (a, b) => parseFloat(b.price) - parseFloat(a.price))
